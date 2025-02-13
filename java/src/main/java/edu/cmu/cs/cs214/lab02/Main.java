a500ba4a7aec6f48e4ea09fd71ba9c5c137d869c
Anka <zanhaa56@gmail.com>
2025-02-13 22:24:57 +0800

    buh zuil ajillaj baigaa

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,17 +1,31 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
-public class Main {
+public final class Main {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Main() {
+        throw new UnsupportedOperationException("Utility class");
+    }
 
     /**
      * @param args heseg.
      */
     public static void main(final String[] args) {
+        final double rectHeight = 2;
+        final double rectWidth = 3;
+        final double squareSide = 5;
+        final double circleRadius = 7;
 
-        Shape rectangle = new Rectangle(2, 3);
-        Shape square = new Square(5);
-        Shape circle = new Circle(7);
+        Shape rectangle = new Rectangle(rectHeight, rectWidth);
+        Shape square = new Square(squareSide);
+        Shape circle = new Circle(circleRadius);
 
         Renderer rectRender = new Renderer(rectangle);
         Renderer squareRender = new Renderer(square);
